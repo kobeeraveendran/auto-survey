aa43f15fb843eb7255fcb5bee2f9fe05d5cfb92e
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-02-09 21:04:02 -0500

    add pdf downloading back, clean up glob pattern

--- arxiv_query.py
+++ arxiv_query.py
@@ -19,12 +19,15 @@ for i, paper in enumerate(results()):
     pdf_filename = paper_info['url'].split('/')[-1]
 
     print(paper_info)
+    print(pdf_filename)
 
     os.makedirs('./downloads', exist_ok = True)
 
     # download pdf versions
-    #arxiv.arxiv.download(paper, dirpath = './downloads/')
-    pdf_file = glob.glob(os.path.join('downloads', '{}*'.format(pdf_filename)))[0]
+
+    # NOTE: not needed for subsequent identical queries
+    arxiv.arxiv.download(paper, dirpath = './downloads/')
+    pdf_file = glob.glob(os.path.join('downloads', '{}*.pdf'.format(pdf_filename)))[0]
     print("pdf filename: ", pdf_file)
     text = pdfminer.high_level.extract_text(pdf_file)
     
