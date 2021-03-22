3185e5526925924eacc6cb51e9c1fb4d324ee606
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-03-21 20:22:51 -0400

    add sentence segmentation

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,21 +1,33 @@
 import pdfminer.high_level
 import spacy
-import string
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-def pdf2text(path):
+def clean_text(path, spacy_model):
+    '''
+    path (str): path to PDF file from which text is extracted
+    '''
+
+    # extract text from pdf @ path
     text = pdfminer.high_level.extract_text(path)
 
+    doc = spacy_model(text)
+
+    # sentence segmentation
+    assert doc.has_annotation("SENT_START")
+
+    for i, sentence in enumerate(doc.sents):
+        print("SENT {}: '{}'".format(i + 1, sentence.text))
+
+    return
     # basic cleaning
     text = text.split(' ')
-    text = [s for s in text if s.isalpha()]
+    text = [s.lower() for s in text if s.isalpha() or '.' in s]
 
+    # non-eng removal
     for i, t in enumerate(text):
         text[i] = t.encode(encoding = "ascii", errors = "ignore").decode()
 
     text = ' '.join(text)
-
-    tokenizer = spacy.load("en_core_web_sm")
-    tokens = tokenizer(text)
 
     token_list = [token.text for token in tokens if str(token).isalpha() or token.pos_ not in ['X', 'SYM', 'PUNCT']]
 
@@ -26,9 +38,13 @@ def pdf2text(path):
 
     return text
 
+def vectorize(text, spacy_model):
+    sentences = spacy_model(text)
+
 if __name__ == "__main__":
 
     doc = "../downloads/1201.2240v1.Bengali_text_summarization_by_sentence_extraction.pdf"
     
-    text = pdf2text(doc)
+    nlp = spacy.load("en_core_web_sm")
+    text = clean_text(doc, nlp)
     #print(text)
