9592d3d48b5ab96b1f55a8d58950004828f91c88
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-04-09 23:40:15 -0400

    add overall summary ROUGE scores, increase topic count for topic dist modeling on entire corpus

--- preprocessing/pipeline.py
+++ preprocessing/pipeline.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
 
 
     # --- Train LDA Model over entire corpus ---
-    NUM_TOPICS = 20
+    NUM_TOPICS = 200
     BATCH_SIZE = 16
     MAX_ITERATIONS = 200
 
@@ -416,14 +416,19 @@ if __name__ == "__main__":
 
     print("Generating multi-document summary... Complete")
 
-    
+    overall_summary_text = []
     if VERBOS:
         print("Overall Summary:")
         for sentence in overall_summary:
             print("    ", sentence[0], " [", sentence[1], "]")
+            overall_summary_text.append(sentence[0])
         print("")
 
+    overall_summary_text = '. '.join(overall_summary_text)
+
     print("Alternative multi-doc summary:\n")
+
+    overall_summary_alt = []
     # alt. multi-doc summary using 2 most relevant sentences of each per-doc summary
     for ds in doc_summaries:
         best_sents = sorted(ds, key = lambda sent: sent[1], reverse = True)
@@ -431,8 +436,11 @@ if __name__ == "__main__":
         for i in range(2):
             sent = best_sents[i]
             print("    {} | [{}]".format(sent[0], sent[1]))
+            overall_summary_alt.append(sent[0] + '.')
 
         print()
+
+    overall_summary_alt_text = ' '.join(overall_summary_alt)
 
     # TextRank summaries for comparison
     # overall_text = ' '.join(overall_sentences)
@@ -442,6 +450,8 @@ if __name__ == "__main__":
     print("Computing per-document ROUGE scores...", end='\r')
     rouge = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
     scores = []
+
+    overall_text = []
     for i in range(len(targets)):
         print("Computing per-document ROUGE scores... ", i, "/", len(targets), end="\r")
 
@@ -454,6 +464,7 @@ if __name__ == "__main__":
 
         # textrank summary for comparison
         doc_text = '. '.join([sentence[1] for sentence in doc_sentences])
+        overall_text.append(doc_text)
         tr_doc_summary = summarize(doc_text, ratio = 0.005)
 
         summary_raw = ""
@@ -467,9 +478,23 @@ if __name__ == "__main__":
         scores.append(sc)
     print("Computing per-document ROUGE scores... Complete           ")
 
+    overall_text = '. '.join(overall_text)
+    
+    print("Generating TextRank comparison summary...", end = '\r')
+    tr_overall_summary = summarize(overall_text, ratio = 0.005)
+    print("Generating TextRank comparison summary... Complete           ")
+
+    print("Computing overall summary ROUGE scores (aggregate and alternative)...", end = '\r')
+    overall_rouge_sc = rouge.score(overall_summary_text, tr_overall_summary)
+    overall_alt_rouge_sc = rouge.score(overall_summary_alt_text, tr_overall_summary)
+    print("Computing overall summary ROUGE scores... Complete                   ")
+
     if VERBOS:
         print("ROUGE Scores:")
         for i, score in enumerate(scores):
             print("Target", i, ":  ROUGE-1 =", score['rouge1'].precision, " ROUGE-L =", score['rougeL'].precision)
+
+        print("Overall  : ROUGE-1 = {} | ROUGE-L = {}".format(overall_rouge_sc["rouge1"].precision, overall_rouge_sc["rougeL"].precision))
+        print("Alt      : ROUGE-1 = {} | ROUGE-L = {}".format(overall_alt_rouge_sc["rouge1"].precision, overall_alt_rouge_sc["rougeL"].precision))
 
 
